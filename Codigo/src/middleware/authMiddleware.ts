9c16db19d4193b940a2f4763169211419a3b8b2b
Gabriel Estevão <gensobrinho@hotmail.com>
2023-10-16 19:45:29 -0300

    feature: middleware de verificação de Token de acesso

---
+++ Codigo/src/middleware/authMiddleware.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import config from 'config';
+
+export interface IRequestWithUser extends Request {
+    user: any;
+  }
+  
+
+export const authMiddleware = (req: IRequestWithUser, res: Response, next: NextFunction) => {
+  const token = req.header('Authorization')?.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Access denied. No token provided.' });
+  }
+
+  try {
+    const jwtSecret = config.get<string>('jwt');
+    const decoded = jwt.verify(token, jwtSecret);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid token.' });
+  }
+};
