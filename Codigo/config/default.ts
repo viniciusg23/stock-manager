52d11c2f022d9ef77733bb681c36b4a4df7ef287
diego <diegomachadocordeiro@gmail.com>
2023-11-14 00:10:38 -0300

    subir ata e teste

--- Codigo/config/default.ts
+++ Codigo/config/default.ts
@@ -1,13 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD
-const env = process.env.ENV ? process.env.ENV.replace(/\s/g, "") : "development";
+const dbPassword = process.env.DB_PASSWORD;
+const env = process.env.ENV
+  ? process.env.ENV.replace(/\s/g, "")
+  : "development";
 const jwt = process.env.JWT_SECRET;
 
 export default {
-    port: 3000,
-    dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.p6dcaiz.mongodb.net/${env}?retryWrites=true&w=majority`,
-    env: env,
-    jwt: jwt
+  port: 3001,
+  dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.p6dcaiz.mongodb.net/${env}?retryWrites=true&w=majority`,
+  env: env,
+  jwt: jwt,
 };
